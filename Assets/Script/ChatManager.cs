369ff0d4a6fce96f15eabad93262a014c04a7e84
mywatt <padamu1@naver.com>
2022-03-27 20:04:13 +0900

    write code of message

--- Assets/Script/ChatManager.cs
+++ Assets/Script/ChatManager.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[System.Serializable]
+public class Data
+{
+    public string type;
+    public string data;
+}
 public class ChatManager : MonoBehaviour
 {
-   
+    private InputField mainInputField;
+    Data positionData;
+    void LockInput(InputField input)
+    {
+        Data data = new Data();
+        if(input.text.Length>0)
+        {
+            data.type = "Ã¤ÆÃ";
+            data.data = input.text;
+            SocketManager.m_Socket.Send(JsonUtility.ToJson(data));
+            input.text = "";
+        }
+        Debug.Log("Enter");
+    }
+
+    void Awake()
+    {
+        mainInputField = this.GetComponent<InputField>();
+    }
+
+    public void Start()
+    {
+        mainInputField.onEndEdit.AddListener(var => { 
+            if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Return))
+                LockInput(mainInputField); 
+        });
+    }
+
 }
 
--- Assets/Script/SocketManager.cs
+++ Assets/Script/SocketManager.cs
@@ -2,16 +2,38 @@ using UnityEngine;
 using System.Collections;
 using WebSocketSharp;
 using System;
+
+[System.Serializable]
+public class PositionData
+{
+    public string type;
+    public float x;
+    public float y;
+    public float z;
+}
+
 public class SocketManager : MonoBehaviour
 {
-    public WebSocketSharp.WebSocket m_Socket = null;
+    public static WebSocketSharp.WebSocket m_Socket = null;
+    PositionData positionData;
     private void Start()
     {
+        positionData = new PositionData();
+        positionData.type = "위치정보";
         m_Socket = new WebSocketSharp.WebSocket("ws://localhost:3000");
         m_Socket.OnMessage += Recv;
         m_Socket.Connect();
     }
-
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            positionData.x = 0.1f;
+            positionData.y = 0.1f;
+            positionData.z = 0.1f;
+            m_Socket.Send(JsonUtility.ToJson(positionData));
+        }
+    }
     public void Connect()
     {
 
@@ -20,7 +42,5 @@ public class SocketManager : MonoBehaviour
     public void Recv(object sender, MessageEventArgs e)
     {
         Debug.Log(e.Data);
-
     }
-
 }
